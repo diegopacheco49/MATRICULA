ae88f6a3051562ae4f1ac071239215cd87371f31
Usuario <Usuario@DESKTOP-6K7J2FD>
2018-12-08 18:11:45 -0500

    init commit

--- src/Coneccion/sistema.java
+++ src/Coneccion/sistema.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 
 public class sistema{
     static Connection con = null;
-    
+    //MODIF
     public Connection coneccion(){
         try {
             String url = "jdbc:sqlserver://DESKTOP-5S44I5P\\MSSQLSERVER4:1433;databaseName=sgmbd";
